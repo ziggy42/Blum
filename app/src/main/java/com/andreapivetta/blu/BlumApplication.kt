68b0acb1e86bdbef8ea0d528b72d78d0a0b462dd
ziggy42 <vanpivix@gmail.com>
2016-05-20 21:57:28 +0200

    Stetho removed

--- app/src/main/java/com/andreapivetta/blu/BlumApplication.kt
+++ app/src/main/java/com/andreapivetta/blu/BlumApplication.kt
@@ -2,7 +2,6 @@ package com.andreapivetta.blu
 
 import android.app.Application
 import com.andreapivetta.blu.twitter.TwitterUtils
-import com.facebook.stetho.Stetho
 import timber.log.Timber
 
 /**
@@ -12,13 +11,8 @@ class BlumApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         TwitterUtils.init(applicationContext)
-        // TODO move me to debug version only
         Timber.plant(Timber.DebugTree());
-
-        if(BuildConfig.DEBUG)
-            Stetho.initializeWithDefaults(this)
     }
 
 }
