7165c4416f447fa07374eaeed651f0cbd2a18926
andrea <vanpivix@gmail.com>
2016-09-20 22:32:47 +0200

    getBitmapFromURL test

---
+++ app/src/androidTest/java/com/andreapivetta/blu/common/utils/UtilsTest.kt
@@ -0,0 +1,21 @@
+package com.andreapivetta.blu.common.utils
+
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Created by andrea on 20/09/16.
+ */
+class UtilsTest {
+
+    private val testUrl = "http://the-toast.net/wp-content/uploads/2015/07/Futurama_fry_looking_squint2.jpg"
+
+    @Test
+    fun getBitmapFromURL() {
+        val bitmap = Utils.getBitmapFromURL(testUrl)
+        assertNotNull(Utils.getBitmapFromURL(testUrl))
+        assertTrue(bitmap!!.byteCount > 0)
+    }
+
+}
