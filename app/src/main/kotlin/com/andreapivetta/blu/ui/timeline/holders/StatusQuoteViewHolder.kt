6de56e573465faaad8d7e026957f25a352ea32a5
andrea <vanpivix@gmail.com>
2016-10-08 21:14:40 +0200

    User screenName in quoted tweet

--- app/src/main/kotlin/com/andreapivetta/blu/ui/timeline/holders/StatusQuoteViewHolder.kt
+++ app/src/main/kotlin/com/andreapivetta/blu/ui/timeline/holders/StatusQuoteViewHolder.kt
@@ -12,6 +12,7 @@ class StatusQuoteViewHolder(container: View, listener: InteractionListener) :
         StatusViewHolder(container, listener) {
 
     private val quotedUserNameTextView = container.quotedUserNameTextView
+    private val quotedUserScreenNameTextView = container.quotedUserScreenNameTextView
     private val quotedStatusTextView = container.quotedStatusTextView
     private val photoImageView = container.photoImageView
     private val quotedStatusLinearLayout = container.quotedStatus
@@ -22,6 +23,7 @@ class StatusQuoteViewHolder(container: View, listener: InteractionListener) :
         if (tweet.quotedStatus) {
             val quotedStatus = tweet.getQuotedTweet()
             quotedUserNameTextView.text = quotedStatus.user.name
+            quotedUserScreenNameTextView.text = "@${quotedStatus.user.screenName}"
 
             if (quotedStatus.mediaEntities.size > 0) {
                 photoImageView.visible()
