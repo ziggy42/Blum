dace32caa1c15a6d7663a7ad21262f419e332e1a
andrea <vanpivix@gmail.com>
2016-10-25 22:22:37 +0200

    Serialize just a sublist of the timeline to avoid TransactionTooLargeException

--- app/src/main/kotlin/com/andreapivetta/blu/ui/timeline/TimelineFragment.kt
+++ app/src/main/kotlin/com/andreapivetta/blu/ui/timeline/TimelineFragment.kt
@@ -105,7 +105,8 @@ open class TimelineFragment : Fragment(), TimelineMvpView, InteractionListener {
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.putSerializable(TAG_TWEET_LIST, adapter.mDataSet as Serializable)
+        outState?.putSerializable(TAG_TWEET_LIST, adapter.mDataSet.subList(0, 30)
+                .toMutableList() as Serializable)
         outState?.putInt(TAG_PAGE, presenter.page)
         super.onSaveInstanceState(outState)
     }
